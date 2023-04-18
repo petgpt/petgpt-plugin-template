d55fb2cbe12e26b7bd40b9a56601174d3b64709f
AHGGG <ironpan3160899@gmail.com>
2023-04-18 17:30:29 +0800

    fix: 修复log信息错误

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -6,17 +6,17 @@ export class Log {
         this.ctx = ctx
     }
     public info(str: string, ...args: any[]) {
-        this.ctx.logger.info(`[plugin] [chatgpt] ${str}`, args)
+        this.ctx.logger.info(`[plugin] [template] ${str}`, args)
     }
     public error(...args: any[]) {
-        this.ctx.logger.error(`[plugin] [chatgpt] ${args}`)
+        this.ctx.logger.error(`[plugin] [template] ${args}`)
     }
 
     public warn(...args: any[]) {
-        this.ctx.logger.warn(`[plugin] [chatgpt] ${args}`)
+        this.ctx.logger.warn(`[plugin] [template] ${args}`)
     }
 
     public debug(...args: any[]) {
-        this.ctx.logger.debug(`[plugin] [chatgpt] ${args}`)
+        this.ctx.logger.debug(`[plugin] [template] ${args}`)
     }
 }
